7f58e72d724c0623237575a838b6f3c3cf7613f0
Oleksandr Chekmez <chekmez@nikama.com>
2016-06-02 09:59:09 +0300

    #118 fix issue with SQL driver loading

--- ODataRestierDynamic/ODataRestierDynamic.Tests/Controllers/DynamicControllerTests.cs
+++ ODataRestierDynamic/ODataRestierDynamic.Tests/Controllers/DynamicControllerTests.cs
@@ -19,7 +19,17 @@ namespace ODataRestierDynamic.Controllers.Tests
 	{
 		private HttpClient client;
 
-		public DynamicControllerTests()
+        public void FixEfProviderServicesProblem()
+        {
+            //The Entity Framework provider type 'System.Data.Entity.SqlServer.SqlProviderServices, EntityFramework.SqlServer'
+            //for the 'System.Data.SqlClient' ADO.NET provider could not be loaded. 
+            //Make sure the provider assembly is available to the running application. 
+            //See http://go.microsoft.com/fwlink/?LinkId=260882 for more information.
+
+            var instance = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
+        }
+
+        public DynamicControllerTests()
 		{
 			var configuration = new HttpConfiguration();
 			configuration.MapRestierRoute<DynamicApi>("DynamicApi", "DynamicApi").Wait();
--- ODataRestierDynamic/ODataRestierDynamic.Tests/Controllers/MediaDataControllerTests.cs
+++ ODataRestierDynamic/ODataRestierDynamic.Tests/Controllers/MediaDataControllerTests.cs
@@ -21,7 +21,17 @@ namespace ODataRestierDynamic.Controllers.Tests
 	{
 		private HttpClient client;
 
-		public MediaDataControllerTests()
+        public void FixEfProviderServicesProblem()
+        {
+            //The Entity Framework provider type 'System.Data.Entity.SqlServer.SqlProviderServices, EntityFramework.SqlServer'
+            //for the 'System.Data.SqlClient' ADO.NET provider could not be loaded. 
+            //Make sure the provider assembly is available to the running application. 
+            //See http://go.microsoft.com/fwlink/?LinkId=260882 for more information.
+
+            var instance = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
+        }
+
+        public MediaDataControllerTests()
 		{
 			var configuration = new HttpConfiguration();
 			Task<System.Web.OData.Routing.ODataRoute> odataRoute = configuration.MapRestierRoute<DynamicApi>("DynamicApi", "DynamicApi");
